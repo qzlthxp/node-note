d99c989c125a8c06dadafd63ae18806205c7faf1
qzlthxp <cc@cMac-mini.local>
2023-06-23 11:33:32 +0800

    edit

--- code/2-how-node-works/modules.js
+++ code/2-how-node-works/modules.js
@@ -1,2 +1,24 @@
-console.log(arguments)
-console.log(require('module').wrapper)
+// console.log(arguments);
+// console.log(require("module").wrapper);
+
+// module.exports
+const C = require('./test-module-1')
+const calc1 = new C()
+console.log(calc1.add(2, 5))
+
+// exports
+// const calc2 = require("./test-module-2");
+const { add, multiply } = require('./test-module-2')
+console.log(multiply(2, 5))
+
+// caching
+/**
+ * Hello from the module 只会执行一次
+ * 因为有缓存只会引入一次模块
+ * require('./test-module-3')() 掉了三次所以执行三次
+ * require('./test-module-3')这个就相当于一个值了
+ * require('./test-module-3')第二次和第三次调用也是从缓存中取值的
+ */
+require('./test-module-3')()
+require('./test-module-3')()
+require('./test-module-3')()
--- code/2-how-node-works/test-module-1.js
+++ code/2-how-node-works/test-module-1.js
@@ -1,1 +1,27 @@
-class Calculator {}
+// class Calculator {
+//   add(a, b) {
+//     return a + b;
+//   }
+
+//   multiply(a, b) {
+//     return a * b;
+//   }
+
+//   divide(a, b) {
+//     return a / b;
+//   }
+// }
+
+module.exports = class {
+  add(a, b) {
+    return a + b
+  }
+
+  multiply(a, b) {
+    return a * b
+  }
+
+  divide(a, b) {
+    return a / b
+  }
+}
